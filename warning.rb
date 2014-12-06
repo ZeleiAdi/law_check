60a3653d9cf6f123ad5520c2e0595665ee30b605
Adam Zelei <za1111@hszk.bme.hu>
2014-12-06 15:48:16 +0100

    more declarative variables, fixed printin

--- warning.rb
+++ warning.rb
@@ -1,6 +1,6 @@
-def warning(what, *where)
-  warn("\e[31m#{what}\e[0m")
-  where.each do
-    warn("\e[33m#{where}\e[0m")
+def warning(message, *occurrences)
+  warn("\e[31m#{message}\e[0m")
+  occurrences.each do |occurrence|
+    warn("\e[33m#{occurrence}\e[0m")
   end
 end
