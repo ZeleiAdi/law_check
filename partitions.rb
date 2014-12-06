f96260944cbc2ce20013c51a716aa65b703e879a
Adam Zelei <za1111@hszk.bme.hu>
2014-12-06 16:40:18 +0100

    path contradiction checking and subset checking methods added

--- partitions.rb
+++ partitions.rb
@@ -22,7 +22,7 @@ module LawCheck
       @partitions.any? { |partition| partition.include? subset_name }
     end
 
-    private def create_toplevel(partition_name)
+    def create_toplevel(partition_name)
       @partitions += [Partition.new([partition_name], true)]
     end
 
@@ -53,6 +53,53 @@ module LawCheck
       create(partition, path)
 
       self
+    end
+
+    def valid_path?(path)
+      if path.empty?
+        true
+      else
+        subset = partition_of_subset(path.first_name)
+        if subset.nil?
+          false
+        else
+          subset[path.first_name].valid_path?(path.rest_names)
+        end
+      end
+    end
+
+    # relies on the fact that path1 and path2 are both valid paths
+    def contradict?(path1, path2)
+      if path1.empty? or path2.empty?
+        false
+      else
+        if path1.first_name == path2.first_name
+          partitions_of_subset(path1.first_name).contradict?(path1.rest_names, path2.rest_names)
+        else
+          if partition_of_subset(path1.first_name) != partition_of_subset(path2.first_name)
+            false
+          else
+            true
+          end
+        end
+      end
+    end
+
+    # relies on the fact that superset_path and set_path are both valid paths
+    def superset?(superset_path: superset_path, set_path: set_path)
+      if set_path.empty?
+        true
+      else
+        if superset_path.empty?
+          false
+        else
+          if superset_path.first_name == set_path.first_name
+            partitions_of_subset(superset_path.first_name).superset?(superset_path: superset_path.rest_names, set_path: set_path.rest_names)
+          else
+            false
+          end
+        end
+      end
     end
   end
 end
